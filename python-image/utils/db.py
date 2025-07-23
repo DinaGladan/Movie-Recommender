92722f21d51d08cdb9b5322e1fcf9781f2e91528
DinaGladan <dina.gladan@gmail.com>
2025-07-24 01:28:59 +0200

    Dodana konekcija s bazom i pokusaj scrapera

---
+++ python-image/utils/db.py
@@ -0,0 +1,27 @@
+# za povzivanje s postgreSQL
+import psycopg2
+import os
+
+# kao i selenium, instalirala sam ga i spremila u requirements.txt
+# ali ga ne prepoznaje
+from dotenv import load_dotenv
+
+load_dotenv()  # ucitavamo podatke iz .env-a
+
+
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=os.getenv("DB_PORT"),
+            database=os.getenv("DB_NAME"),
+        )
+        return conn
+    except psycopg2.Error as error:
+        print(f"Error connecting to PostgreSQL {error}")
+
+
+def close_db_connection(conn):
+    return conn.close()
--- python-image/utils/scraper.py
+++ python-image/utils/scraper.py
@@ -1,7 +1,28 @@
-from bs4 import BeautifulSoup
-import requests
+# from bs4 import BeautifulSoup
+# import requests
 
-page_to_scrape = requests.get("https://letterboxd.com/film/barbie/")
-soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
-movies = soup.findAll()
+# page_to_scrape = requests.get("https://letterboxd.com/films/popular/")
+# soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+
+# # movies = soup.findAll("div", attrs={"class":"name js-widont prettify"})
+# movies = soup.select(
+#     "div.film-poster"
+# )  # uzmi sve one divove koji imaju klasu film-poster
+# for movie in movies:
+#     title = movie.get("data-film-name")
+#     print(title)
+
+# from bs4 import BeautifulSoup
+# import requests
+
+# url = "https://archive.org/details/movies"
+# page = requests.get(url)
+# soup = BeautifulSoup(page.text, "html.parser")
+
+
+# movies = soup.select("div.item-ttl a")
+
+# for movie in movies:
+#     title = movie.text.strip()  # Uzimamo tekst linka (naziv filma)
+#     print(title)
